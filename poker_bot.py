4e29c3326e66b0845324e4397afcaf3768422fda
rosa15giugno-collab <rosa15giugno@gmail.com>
2026-06-06 12:12:13 +0200

    Update poker_bot.py

--- poker_bot.py
+++ poker_bot.py
@@ -271,9 +271,10 @@ def main():
 
     app.run_webhook(
     listen="0.0.0.0",
-    port=PORT,
+    port=int(os.environ.get("PORT", 8080)),
+    webhook_url=f"{WEBHOOK_URL}/webhook",
     url_path="webhook",
-    webhook_url=f"{WEBHOOK_URL}/webhook"
+    drop_pending_updates=True
 )
 
 if __name__ == "__main__":
