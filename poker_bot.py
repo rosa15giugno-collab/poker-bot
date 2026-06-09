9db004e058b4898d231399c9003cc42093f8f3ee
rosa15giugno-collab <rosa15giugno@gmail.com>
2026-06-09 10:28:54 +0200

    Update poker_bot.py

--- poker_bot.py
+++ poker_bot.py
@@ -184,11 +184,11 @@ async def slot(update, context):
     await q.message.reply_text(
     f"""
 ╔══════════════╗
-║       🎰 SLOT 🎰           ║
+║        🎰 SLOT 🎰           ║
 ╚══════════════╝
 
 ┏━━━━━━━━━━━━━┓
-┃    {r[0]} │ {r[1]} │ {r[2]}         ┃
+┃     {r[0]} │ {r[1]} │ {r[2]}         ┃
 ┗━━━━━━━━━━━━━┛
 
 💰 Vincita: {win}
