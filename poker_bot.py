b93e133b95887cfd679a48a8cfa9e4809e138375
rosa15giugno-collab <rosa15giugno@gmail.com>
2026-06-11 11:10:30 +0200

    Update poker_bot.py

--- poker_bot.py
+++ poker_bot.py
@@ -161,11 +161,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     caption=
     "     Benvenuto in 👑 Casinò by Rosa \n\n"
     "𝑰𝒍 𝒄𝒂𝒔𝒐 𝒏𝒐𝒏 è 𝒄𝒂𝒐𝒔:è 𝒖𝒏 𝒍𝒊𝒏𝒈𝒖𝒂𝒈𝒈𝒊𝒐…\n"
-    "    …𝒄𝒉𝒊 𝒔𝒂 𝒂𝒔𝒄𝒐𝒍𝒕𝒂𝒓𝒍𝒐 𝒗𝒊𝒏𝒄𝒆\n"
-    "            ✦ ───── ✦\n"
+    "       …𝒄𝒉𝒊 𝒔𝒂 𝒂𝒔𝒄𝒐𝒍𝒕𝒂𝒓𝒍𝒐 𝒗𝒊𝒏𝒄𝒆\n"
+    "               ✦ ───── ✦\n"
     "      Slot, Blackjack, Roulette\n"
     "       Classifiche settimanali\n"
-    "            ✦ ───── ✦\n"
+    "               ✦ ───── ✦\n"
     "𝑰𝒍 𝑫𝒆𝒔𝒕𝒊𝒏𝒐 𝒕𝒊 𝒈𝒖𝒂𝒓𝒅𝒂. 𝑻𝒖 𝒈𝒖𝒂𝒓𝒅𝒊 𝒍𝒖𝒊"
     "👇 Scegli una modalità",
     reply_markup=menu()
